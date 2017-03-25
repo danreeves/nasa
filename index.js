8e30cb674cdd807744d98d5bb94203590d199322
Dan Reeves <heydanreeves@gmail.com>
2017-03-25 11:28:31 +0000

    ~~~

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const { parse: parseUrl } = require('url');
 const { renderSync } = require('node-sass');
 const { encode } = require('he');
 const normalise = require('normalize-uri');
-const parse = require('urlencoded-body-parser')
+const parseBody = require('urlencoded-body-parser')
 
 // Read style.scss from the filesystem into a string
 const scss = fs.readFileSync('./style.scss', 'utf8');
@@ -23,15 +24,33 @@ const html = fs.readFileSync('./index.html', 'utf8').replace('{{ css }}', css);
 // This is the function that is called when
 //  someone visits the site.
 module.exports = async (req, res) => {
+    // If the form has been submit
     if (req.method === 'POST') {
-        const data = await parse(req);
+        // Get the form data
+        const data = await parseBody(req);
+        // If the txt field isn't empty
         if (data.txt) {
+            // Redirect to that value as a path
             res.writeHead(301, {
-                Location: `/${data.txt}`,
+                Location: `/${encodeURIComponent(data.txt)}`,
+            });
+            res.end();
+        }
+    } else if (req.method === 'GET') {
+        // Get query parameters out of the URL
+        const url = parseUrl(req.url, true);
+        // If there is ?txt and it has a value
+        if (url.query && url.query.txt) {
+            // Redirect to that value as a path
+            res.writeHead(301, {
+                Location: `/${encodeURIComponent(url.query.txt)}`,
             });
             res.end();
         }
     }
+
+    // If none of the above matches we want to take the URL
+    // do some transforms and replace it into the html
     let path;
     try {
         path = decodeURIComponent(normalise(req.url))
